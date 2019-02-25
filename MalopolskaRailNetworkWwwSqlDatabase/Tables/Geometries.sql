d4e32de0e2e9fc8614241fa4f0003a2bfa1cd58f
Staszek <stanislaw.polanski@hotmail.com>
2019-02-25 22:37:03 +0100

    #1 Developed Geometries and RailwayUnits

---
+++ MalopolskaRailNetworkWwwSqlDatabase/Tables/Geometries.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Geometries]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Value] [sys].[geography] NOT NULL
+)
---
+++ MalopolskaRailNetworkWwwSqlDatabase/Tables/RailwayUnits.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RailwayUnits]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [GeometriesId] INT NOT NULL UNIQUE, 
+    CONSTRAINT [FK_RailwayUnits_ToGeometries] FOREIGN KEY ([GeometriesId]) REFERENCES [Geometries]([Id])
+)
