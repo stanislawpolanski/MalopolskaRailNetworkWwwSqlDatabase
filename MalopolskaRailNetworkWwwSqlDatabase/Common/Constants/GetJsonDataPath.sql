c11bc9a2d12d48a4e307163e02d2670e587aef06
Staszek <stanislaw.polanski@hotmail.com>
2019-03-02 19:59:15 +0100

    #5 Paths constants functions established

---
+++ MalopolskaRailNetworkWwwSqlDatabase/Common/Constants/GetJsonDataPath.sql
@@ -0,0 +1,9 @@
+﻿CREATE FUNCTION [dbo].[GetJsonDataPath]
+(
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @RootPath NVARCHAR(150) = (SELECT dbo.GetProjectRootPath()) + '\SeedingData\RawJsonData'
+	RETURN @RootPath
+END
---
+++ MalopolskaRailNetworkWwwSqlDatabase/Common/Constants/GetProjectRootPath.sql
@@ -0,0 +1,8 @@
+﻿CREATE FUNCTION [dbo].[GetProjectRootPath]
+(
+)
+RETURNS NVARCHAR(150)
+AS
+BEGIN
+	RETURN 'C:\Users\Dell\source\repos\MalopolskaRailNetworkWwwSqlDatabase\MalopolskaRailNetworkWwwSqlDatabase'
+END
