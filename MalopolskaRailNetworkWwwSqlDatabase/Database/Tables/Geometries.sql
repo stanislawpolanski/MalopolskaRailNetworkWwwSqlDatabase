b53ed39d7dfbc1116df91f9674135b6cc4446d52
Staszek <stanislaw.polanski@hotmail.com>
2019-03-02 16:27:59 +0100

    #11 Deleted disallowed key in Geometries

--- MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Geometries.sql
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Geometries.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[Geometries]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Value] [sys].[geography] NOT NULL, 
-    CONSTRAINT [AK_Geometries_Value] UNIQUE ([Value])
+    [Value] [sys].[geography] NOT NULL
 )
