ac3af26d1c474a7f4562100d43353d320bd63749
Staszek <stanislaw.polanski@hotmail.com>
2019-03-03 14:45:46 +0100

    #12 Folders rearranged, jsons updated

---
+++ MalopolskaRailNetworkWwwSqlDatabase/SeedingData/Parse/ParseGeometries.sql
@@ -0,0 +1,25 @@
+﻿--CREATE FUNCTION [dbo].[Function1]
+--(
+--	@param1 int,
+--	@param2 char(5)
+--)
+--RETURNS @returntable TABLE
+--(
+--	c1 int,
+--	c2 char(5)
+--)
+--AS
+--BEGIN
+--	INSERT @returntable
+--	SELECT @param1, @param2
+--	RETURN
+--END
+
+declare @json nvarchar(max) = dbo.ReadGeometryJson()
+
+select * from
+	openjson(@json)
+	with (
+		Id int,
+		Value nvarchar(max)
+		)
---
+++ MalopolskaRailNetworkWwwSqlDatabase/dbo/Functions/GetRawJsonData.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[GetRawJsonData]
+(
+)
+RETURNS @returntable TABLE
+(
+	Name nvarchar(max),
+	Value nvarchar(max)
+)
+AS
+BEGIN
+	insert into @returntable (Name)
+	values 
+		('Geometries_Dataset.json'),
+		('Owners_Dataset.json'),
+		('Railways_Dataset.json'),
+		('RailwayUnits_Dataset.json'),
+		('RollingStock_Dataset.json'),
+		('Stations_Dataset.json'),
+		('StationsToGeomteries_Dataset.json'),
+		('TypesOfAPoint_Dataset.json')
+	RETURN
+END
