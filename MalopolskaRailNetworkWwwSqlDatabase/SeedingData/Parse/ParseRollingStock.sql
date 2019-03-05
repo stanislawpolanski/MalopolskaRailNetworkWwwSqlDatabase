d2164699b13cd0bfe8aeaa06fd4c0238f0570a9b
Staszek <stanislaw.polanski@hotmail.com>
2019-03-05 17:54:00 +0100

    #12 Rolling stock parsed

---
+++ MalopolskaRailNetworkWwwSqlDatabase/SeedingData/Parse/ParseRollingStock.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[ParseRollingStock]
+(
+)
+RETURNS @returntable TABLE
+(
+	Id int,
+	OwnerId int,
+    Name nvarchar(50)
+)
+AS
+BEGIN
+    declare @json nvarchar(max) = dbo.ReadRollingStockJson();
+    insert into @returntable(Id, OwnerId, Name)
+        select * from openjson(@json)
+        with(Id int N'$.Id', OwnerId int, Name nvarchar(50))
+    return
+END
