5725fdda3f25cbe890bd20cd6cb8f6baf292df09
Staszek <stanislaw.polanski@hotmail.com>
2019-03-19 20:51:06 +0100

    #14 Tables developed, jsons copied to the project

---
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Photos.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Photos]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FilePath] NVARCHAR(50) NOT NULL, 
+    [AdditionDateTime] DATETIME2 NULL, 
+    [Description] NVARCHAR(500) NULL, 
+    CONSTRAINT [CK_Photos_FilePath] CHECK (FilePath LIKE '%.jpg'), 
+    CONSTRAINT [AK_Photos_FilePath] UNIQUE ([FilePath]) 
+)
---
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/PhotosToObjectsOfInterest.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PhotosToObjectsOfInterest]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [ObjectOfInterestId] INT NOT NULL, 
+    [PhotoId] INT NOT NULL, 
+    CONSTRAINT [FK_PhotosToObjectsOfInterest_ToObjectsOfInterest] FOREIGN KEY ([ObjectOfInterestId]) REFERENCES [Stations]([Id]), 
+    CONSTRAINT [FK_PhotosToObjectsOfInterest_ToTablePhotos] FOREIGN KEY ([PhotoId]) REFERENCES [Photos]([Id])
+)
