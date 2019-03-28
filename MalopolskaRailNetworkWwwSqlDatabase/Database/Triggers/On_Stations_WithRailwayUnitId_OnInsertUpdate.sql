2bf577635ba79699355b872987732357b60b845b
Staszek <stanislaw.polanski@hotmail.com>
2019-03-28 17:41:36 +0100

    #29 solution sketch

---
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Triggers/On_Stations_WithRailwayUnitId_OnInsertUpdate.sql
@@ -0,0 +1,12 @@
+﻿CREATE TRIGGER [On_Stations_WithRailwayUnitId_OnInsertUpdate]
+ON [dbo].[StationsToGeometries]
+FOR INSERT, UPDATE
+AS
+BEGIN
+    -- select geometryId from stations to geometries
+    -- if is not null then (else -> end)
+       -- select geometry by geometryId from station to geoms.
+       -- select railway unit id from stations where station id = insert station id
+       -- select railway unit geometry by id
+       -- unit.intersection(station geometry) -> if null then raise error
+END
