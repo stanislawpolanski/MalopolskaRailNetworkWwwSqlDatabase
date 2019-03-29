9444520871d509cc086735e4a366cb90e0a5e2f8
Staszek <stanislaw.polanski@hotmail.com>
2019-03-29 19:51:18 +0100

    #29 task abandoned

--- MalopolskaRailNetworkWwwSqlDatabase/Database/Triggers/On_Stations_WithRailwayUnitId_OnInsertUpdate.sql
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Triggers/On_Stations_WithRailwayUnitId_OnInsertUpdate.sql
@@ -1,12 +1,24 @@
 ﻿CREATE TRIGGER [On_Stations_WithRailwayUnitId_OnInsertUpdate]
 ON [dbo].[StationsToGeometries]
-FOR INSERT, UPDATE
+INSTEAD OF INSERT, UPDATE
 AS
 BEGIN
     -- select geometryId from stations to geometries
+    declare @stationGeometryId int null
+    select @stationGeometryId = inserted.GeometryId
+
+    declare @unitGeometryId int null
+    select @unitGeometryId = inserted.RailwayUnitId
     -- if is not null then (else -> end)
+    if(@stationGeometryId is not null)
+    begin
        -- select geometry by geometryId from station to geoms.
-       -- select railway unit id from stations where station id = insert station id
-       -- select railway unit geometry by id
-       -- unit.intersection(station geometry) -> if null then raise error
+        declare @unitGeometry sys.Geometry
+        select @unitGeometry = DataAccess.Geometries.SpatialData from DataAccess.Geometries
+            left join DataAccess.StationsToGeometries on DataAccess.Geometries.Id = DataAccess.StationsToGeometries.EndingKmpost
+            where DataAccess.Geometries.Id = @
+        -- select railway unit id from stations where station id = insert station id
+        -- select railway unit geometry by id
+        -- unit.intersection(station geometry) -> if null then raise error
+    end
 END
