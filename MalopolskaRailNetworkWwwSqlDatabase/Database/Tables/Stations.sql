ee5c93f68e3100b8324368d10b6ce262dbe3009b
Staszek <stanislaw.polanski@hotmail.com>
2019-03-30 10:25:53 +0100

    #36 Foregin key on id established

--- MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Stations.sql
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Stations.sql
@@ -5,4 +5,5 @@
     [TypeOfAPointId] INT NULL, 
     CONSTRAINT [FK_Stations_ToRailways] FOREIGN KEY ([RailwayUnitId]) REFERENCES [RailwayUnits]([Id]), 
     CONSTRAINT [FK_Stations_ToTypes] FOREIGN KEY ([TypeOfAPointId]) REFERENCES [TypesOfAStation]([Id]), 
+    CONSTRAINT [FK_Stations_ToObjectsOfInterest] FOREIGN KEY ([Id]) REFERENCES [ObjectsOfInterest]([Id]) 
 )
