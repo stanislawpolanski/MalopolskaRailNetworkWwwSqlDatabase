2db2f27ad0ba09d0a4053e9aeebd3c06c31e5fdb
Staszek <stanislaw.polanski@hotmail.com>
2019-03-31 11:00:29 +0200

    #41 column name fixed

--- MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Stations.sql
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Stations.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Stations]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [TypeOfAPointId] INT NULL, 
-    CONSTRAINT [FK_Stations_ToTypes] FOREIGN KEY ([TypeOfAPointId]) REFERENCES [TypesOfAStation]([Id]), 
+    [TypeOfAStationId] INT NULL, 
+    CONSTRAINT [FK_Stations_ToTypes] FOREIGN KEY ([TypeOfAStationId]) REFERENCES [TypesOfAStation]([Id]), 
     CONSTRAINT [FK_Stations_ToObjectsOfInterest] FOREIGN KEY ([Id]) REFERENCES [ObjectsOfInterest]([Id]) 
 )
