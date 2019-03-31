885dd235d65aa1e71df1567d9cf37979db7dea24
Staszek <stanislaw.polanski@hotmail.com>
2019-03-31 11:31:56 +0200

    #41 db deploys with old name

--- MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Stations.sql
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Tables/Stations.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Stations]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [TypeOfAStationId] INT NULL, 
-    CONSTRAINT [FK_Stations_ToTypes] FOREIGN KEY ([TypeOfAStationId]) REFERENCES [TypesOfAStation]([Id]), 
+    [TypeOfAPointId] INT NULL, 
+    CONSTRAINT [FK_Stations_ToTypes] FOREIGN KEY ([TypeOfAPointId]) REFERENCES [TypesOfAStation]([Id]), 
     CONSTRAINT [FK_Stations_ToObjectsOfInterest] FOREIGN KEY ([Id]) REFERENCES [ObjectsOfInterest]([Id]) 
 )
