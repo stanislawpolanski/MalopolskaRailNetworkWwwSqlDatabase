c6a5a3f22462c94750ecc7070b90d0449643e5b2
Staszek <stanislaw.polanski@hotmail.com>
2019-09-07 20:31:15 +0200

    #50 Triggers for deleting related entities

---
+++ MalopolskaRailNetworkWwwSqlDatabase/Database/Triggers/DeleteObjectOfInterest.sql
@@ -0,0 +1,12 @@
+﻿CREATE TRIGGER [DeleteObjectOfInterest]
+    ON [dbo].[ObjectsOfInterest]
+    FOR DELETE
+    AS
+    BEGIN
+        SET NOCOUNT ON
+
+        delete from dbo.PhotosToObjectsOfInterest
+        where exists (select 1 
+            from deleted 
+            where deleted.Id = dbo.PhotosToObjectsOfInterest.ObjectOfInterestId)
+    END
